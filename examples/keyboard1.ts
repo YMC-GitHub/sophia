f7013bd8a8f2ec727b8934452b292d1623fcb5fe
yemiancheng <ymc.github@gmail.com>
2024-06-13 14:59:13 +0800

    code(core): set ts note and slim

--- examples/keyboard1.ts
+++ examples/keyboard1.ts
@@ -3,6 +3,7 @@ import { Keyboard } from '../index'
 const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
 
 async function main() {
+  await sleep(2000)
   await Keyboard.typing('Hello, World!')
 }
 
