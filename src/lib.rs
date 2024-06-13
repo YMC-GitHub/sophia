1e20b719f5f58d20c3e2a1d7f987bf387bed0273
yemiancheng <ymc.github@gmail.com>
2024-06-13 16:32:47 +0800

    code(core): set lib note and slim

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,18 @@
 #![deny(clippy::all)]
 
-/// import the preludes
-// use napi::bindgen_prelude::*;
-use napi_derive::napi;
-#[napi]
-fn fib(n: u32) -> u32 {
-  match n {
-    1 | 2 => 1,
-    _ => fib(n - 1) + fib(n - 2),
-  }
-}
+// code(core): use macro deny(clippy::all)
 
+// code(core): use ./geometry.rs as pub mod geometry
+// code(core): use ./screen.rs as pub mod screen
+// code(core): use ./utils.rs as pub mod utils
+// code(core): use ./fib.rs as pub mod fib
+
+pub mod fib;
 pub mod geometry;
 pub mod screen;
 pub mod utils;
 
+// code(core): use ./win/xx.rs as pub mod win
+// code(core): use macros cfg(target_os = "windows") to label it
 #[cfg(target_os = "windows")]
 pub mod win;
